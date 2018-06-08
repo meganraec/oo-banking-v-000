25c2181d8da915d1fa28a8ea02b351c4f19148c8
Megan Collier <meganraec@live.com>
2018-06-08 02:42:55 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,23 +10,14 @@ class Transfer
   def valid?
     sender.valid? && receiver.valid?
   end
-#     describe '#valid?' do
-#       it "can check that both accounts are valid" do
-#         expect(avi.valid?).to eq (true)
-#         expect(amanda.valid?).to eq(true)
-#         expect(transfer.valid?).to eq(true)
-#       end
-# 
-#       it "calls on the sender and reciever's #valid? methods" do
-#         transfer_class = File.read("lib/transfer.rb")
-# 
-#         expect(amanda).to receive(:valid?).and_return(true)
-#         expect(avi).to receive(:valid?).and_return(true)
-# 
-#         transfer.valid?
-#       end
-#     end
-# 
+
+  def execute_transaction
+    if self.valid?
+    sender.balance -= amount
+    receiver.balance -= amount
+    self.status = "complete"
+  end
+#
 #     describe '#execute_transaction' do
 #       it "can execute a successful transaction between two accounts" do
 #         transfer.execute_transaction
@@ -34,7 +25,7 @@ class Transfer
 #         expect(avi.balance).to eq(1050)
 #         expect(transfer.status).to eq("complete")
 #       end
-# 
+#
 #       it "each transfer can only happen once" do
 #         transfer.execute_transaction
 #         expect(amanda.balance).to eq(950)
@@ -44,13 +35,13 @@ class Transfer
 #         expect(amanda.balance).to eq(950)
 #         expect(avi.balance).to eq(1050)
 #       end
-# 
+#
 #       it "rejects a transfer if the sender doesn't have a valid account" do
 #         expect(bad_transfer.execute_transaction).to eq("Transaction rejected. Please check your account balance.")
 #         expect(bad_transfer.status).to eq("rejected")
 #       end
 #     end
-# 
+#
 #     describe '#reverse_transfer' do
 #       it "can reverse a transfer between two accounts" do
 #         transfer.execute_transaction
@@ -61,7 +52,7 @@ class Transfer
 #         expect(amanda.balance).to eq(1000)
 #         expect(transfer.status).to eq("reversed")
 #       end
-# 
+#
 #       it "it can only reverse executed transfers" do
 #         transfer.reverse_transfer
 #         expect(amanda.balance).to eq(1000)
